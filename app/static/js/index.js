74445ecfcd681fd2c369e86056a1ce507bbe9408
wanderanimrod <wanderanimrod@gmail.com>
2013-12-09 01:03:47 +0300

    [nim] Extracted sub-templates from swelling index.html

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,4 +1,4 @@
-function drawGraphCanvas() {
+function prepareGraphCanvas() {
     var graphCanvas = $("#graphingArea");
     var visRows = $('.visRow');
     visRows.css('height', graphCanvas.height() / 2);
@@ -6,33 +6,33 @@ function drawGraphCanvas() {
     visPanes.css('height', visRows.height() - 27);
 
     var visualisations = $('.vis');
-
     visualisations
         .css('height', 320)
         .css('background-color', 'teal')
         .css('opacity', 0.5);
 
-    visualisations.hover(function() {
+    fadeVisPanes(visualisations);
+}
+
+function fadeVisPanes(panes) {
+    panes.hover(function () {
         $(this).animate({opacity: 1}, 'slow');
     });
-
-    visualisations.mouseleave(function() {
+    panes.mouseleave(function () {
         $(this).animate({opacity: 0.5}, '0.1s');
     });
 }
 
-
-
 function silenceControlBarItems() {
     $('.objectProperty').css('font-size', 7);
     $('.objectProperties').css('visibility', 'hidden');
-    $('.simObject').hover(function() {
-       $(this).children('.objectProperties').css('visibility', 'visible');
+    $('.simObject').hover(function () {
+        $(this).children('.objectProperties').css('visibility', 'visible');
     });
-    $('.simObject').mouseleave(function() {
-       $(this).children('.objectProperties').css('visibility', 'hidden');
+    $('.simObject').mouseleave(function () {
+        $(this).children('.objectProperties').css('visibility', 'hidden');
     });
 }
 
-drawGraphCanvas();
+prepareGraphCanvas();
 silenceControlBarItems();
