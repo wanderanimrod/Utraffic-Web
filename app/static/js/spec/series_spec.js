96db98bcb7d252cf10b5804025f479951f05ffc6
wanderanimrod <wanderanimrod@gmail.com>
2013-12-16 23:07:22 +0300

    [nim] Completed behaviour testing of isComplete() on series object as it stands now

--- app/static/js/spec/series_spec.js
+++ app/static/js/spec/series_spec.js
@@ -1,6 +1,11 @@
 describe("Series", function () {
-    it("Should say it is complete when its status is 'complete'", function () {
-        var series = new Series({"id": 100, "data": [], "status": "complete"});
+    it("should say it is complete when its status is 'complete'", function () {
+        var series = new Series({"status": "complete"});
         expect(series.isComplete()).toBe(true);
+    });
+
+    it("should say it is not complete when its status is 'active'", function() {
+       var series = new Series({"status": "active"});
+        expect(series.isComplete()).toBe(false);
     });
 });
