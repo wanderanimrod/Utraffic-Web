30ebab9934c293388b7829904b154cf90520b783
wanderanimrod <wanderanimrod@gmail.com>
2013-12-21 17:47:20 +0300

    [nim] Testing that getting visualisation from session by id works. Refactoring tests

--- app/static/js/spec/session_spec.js
+++ app/static/js/spec/session_spec.js
@@ -1,30 +1,35 @@
 describe("Session", function() {
+
+    var session;
+
+    beforeEach(function() {
+        session = new Session();
+    });
+
     it("should have no visualisation upon instantiation", function() {
-        var session = createSession();
         expect(session.visualisations).toEqual([]);
     });
 
     it("should have the activeVisualisation as undefined upon instantiation", function() {
-        var session = createSession();
         expect(session.activeVisualisation).toBe(undefined);
     });
 
     it("should add visualisation to itself", function() {
-        var session = createSession();
         var visualisation = session.addNewVisualisation();
         expect(session.getVisualisation(visualisation.id))
     });
 
     it("should assign sequential ids to visualisation added", function() {
-        var session = createSession();
         var visualisation1 = session.addNewVisualisation();
         var visualisation2 = session.addNewVisualisation();
         var visualisation3 = session.addNewVisualisation();
         expect(visualisation2.id).toBe(visualisation1.id + 1);
         expect(visualisation3.id).toBe(visualisation2.id + 1);
     });
-});
 
-function createSession() {
-    return new Session();
-}
+    it("should get visualisation by id", function() {
+        var visualisation = session.addNewVisualisation();
+        var returnedVisualisation = session.getVisualisation(visualisation.id);
+        expect(returnedVisualisation).toBe(visualisation);
+    });
+});
