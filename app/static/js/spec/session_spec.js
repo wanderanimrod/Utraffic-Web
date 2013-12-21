7d722c0ae54cfb0b2c3bf93171b8b92de9824eaa
wanderanimrod <wanderanimrod@gmail.com>
2013-12-21 17:33:38 +0300

    [nim] Tested that visualisations added to the session get sequential ids

--- app/static/js/spec/session_spec.js
+++ app/static/js/spec/session_spec.js
@@ -14,6 +14,15 @@ describe("Session", function() {
         var visualisation = session.addNewVisualisation();
         expect(session.getVisualisation(visualisation.id))
     });
+
+    it("should assign sequential ids to visualisation added", function() {
+        var session = createSession();
+        var visualisation1 = session.addNewVisualisation();
+        var visualisation2 = session.addNewVisualisation();
+        var visualisation3 = session.addNewVisualisation();
+        expect(visualisation2.id).toBe(visualisation1.id + 1);
+        expect(visualisation3.id).toBe(visualisation2.id + 1);
+    });
 });
 
 function createSession() {
