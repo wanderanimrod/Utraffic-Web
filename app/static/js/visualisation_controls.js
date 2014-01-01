d32a215a7e1e44f16fe307c2251c89b0c57997c8
wanderanimrod <wanderanimrod@gmail.com>
2014-01-01 13:36:56 +0300

    [nim] Extracted visualisation control-related functions from index.js for Single Responsibility and readability

---
+++ app/static/js/visualisation_controls.js
@@ -0,0 +1,60 @@
+$('.showsOnlyOnVis').css('display', 'none');
+$('.visPaneControls').css('padding', '.25em .5em');
+
+function visElementsOfType(visId, type) {
+    return $("#" + visId + type);
+}
+
+function activateAddVisSigns() {
+    $('.addVisSign').click(function() {
+        $(this).css('display', 'none');
+        relativesOfType(".vis.front", $(this)).css('background-image', 'url("/static/images/grey.png")');
+        relativesOfType(".showsOnlyOnVis", $(this)).css('display', '');
+    })
+}
+
+function activateRemoveVisIcons() {
+    $('.removeVis').click(function() {
+        relativesOfType(".showsOnlyOnVis", $(this)).css('display', 'none');
+        relativesOfType(".vis.front", $(this)).css('background-image', '');
+        relativesOfType('.addVisSign', $(this)).css('display', '');
+    })
+}
+
+function activateVisDataIcons() {
+    $('.visData').click(function() {
+        relativesOfType('.vis.front', $(this)).css('z-index', -1);
+        relativesOfType('.vis.back', $(this)).css('z-index', 1);
+    })
+}
+
+function activateBackToVisIcons() {
+    $('.backToVis').click(function() {
+        relativesOfType('.vis.back', $(this)).css('z-index', -1);
+        relativesOfType('.vis.front', $(this)).css('z-index', 1);
+    })
+}
+
+function createVisualisationObjectsForUiElements() {
+    var visualisation = null;
+    $('.vis').each(function() {
+        if($(this).hasClass('front')) {
+            visualisation = session.addNewVisualisation();
+        }
+        $(this).attr('id', visualisation.id);
+    });
+}
+
+function copyVisIdsToAllDescendants() {
+    $('.vis').each(function() {
+        propagateVisIdToAllDescendants($(this));
+    })
+}
+
+function propagateVisIdToAllDescendants(object) {
+    var objectId = object.attr('id');
+    object.children().each(function() {
+        $(this).attr('id', objectId);
+        propagateVisIdToAllDescendants($(this));
+    });
+}
