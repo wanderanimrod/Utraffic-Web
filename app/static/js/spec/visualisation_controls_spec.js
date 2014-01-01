94e6925d85c7617fca7f991fc47192eade86fe6c
wanderanimrod <wanderanimrod@gmail.com>
2014-01-01 23:59:43 +0300

    [nim] Cleaning up

--- app/static/js/spec/visualisation_controls_spec.js
+++ app/static/js/spec/visualisation_controls_spec.js
@@ -1,29 +1,24 @@
 describe("Visualisation Controls", function() {
-    var session = new Session();
+    var session, mockVisCanvas;
+    session = new Session();
     beforeEach(function() {
         session.visualisations = [];
         session.activeVisualisation = null;
+        mockVisCanvas = jQuery('<div class="vis front"></div><div class="vis front"></div>');
+        mockVisCanvas.visElement = function(position) {
+            return jQuery($('.vis.front', mockVisCanvas)[position]);
+        };
     });
 
-    describe("createVisualisationObjectForUiElements", function() {
-        var mockVisCanvas;
-        beforeEach(function() {
-            mockVisCanvas = jQuery('<div class="vis front"></div><div class="vis front"></div>');
-            mockVisCanvas.visElement = function(position) {
-                return jQuery($('.vis.front', mockVisCanvas)[position]);
-            };
-        });
-
-        it("should create a visualisation for each element with 'vis' and 'front' classes", function() {
-            spyOn(jQuery.fn, 'find').andReturn(mockVisCanvas);
-            createVisualisationObjectsForUiElements();
-            expect(session.visualisations.length).toBe(2);
-        });
-        it("should set the id of ui elements to the id of the visualisations created for them", function() {
-            spyOn(jQuery.fn, 'find').andReturn(mockVisCanvas);
-            createVisualisationObjectsForUiElements();
-            var visElementId = parseInt(mockVisCanvas.visElement(0).attr('id'));
-            expect(visElementId).toBe(session.visualisations[0].id);
-        });
+    it("should create a visualisation for each element with 'vis' and 'front' classes", function() {
+        spyOn(jQuery.fn, 'find').andReturn(mockVisCanvas);
+        createVisualisationObjectsForUiElements();
+        expect(session.visualisations.length).toBe(2);
+    });
+    it("should set the id of ui elements to the id of the visualisations created for them", function() {
+        spyOn(jQuery.fn, 'find').andReturn(mockVisCanvas);
+        createVisualisationObjectsForUiElements();
+        var visElementId = parseInt(mockVisCanvas.visElement(0).attr('id'));
+        expect(visElementId).toBe(session.visualisations[0].id);
     });
 });
