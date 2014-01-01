55bcf6b9d7318a51273b79dafa957ac31cf66225
wanderanimrod <wanderanimrod@gmail.com>
2014-01-01 14:05:52 +0300

    [nim] Extracted some visualisation control and series control specs out of index spec

--- app/static/js/spec/index_spec.js
+++ app/static/js/spec/index_spec.js
@@ -1,60 +1,4 @@
 describe("index.js", function() {
-    var session = new Session();
-    beforeEach(function() {
-        session.visualisations = [];
-        session.activeVisualisation = null;
-    });
-
-    describe("Adding Series to Visualisations", function() {
-
-        var visualisation, mockAddSeriesElement;
-        beforeEach(function() {
-            visualisation = session.addNewVisualisation();
-            mockAddSeriesElement = jQuery("<div></div>");
-        });
-
-        describe("startAddingSeries", function() {
-            it("should make the series the active series of the session", function() {
-                startAddingSeries(visualisation, mockAddSeriesElement);
-                expect(session.activeVisualisation).toEqual(visualisation);
-            });
-            it("should set the state of the passed visualisation to 'ADDING_SERIES'", function() {
-                expect(visualisation.state).toBe(visualisationState.IDLE);
-                startAddingSeries(visualisation, mockAddSeriesElement);
-                expect(visualisation.state).toBe(visualisationState.ADDING_SERIES);
-            });
-        });
-
-        describe("finishAddingSeries", function() {
-            it("should set the state of the passed visualisation to 'IDLE'", function() {
-                finishAddingSeries(visualisation, mockAddSeriesElement);
-                expect(visualisation.state).toBe(visualisationState.IDLE);
-            });
-        });
-    });
-
-    describe("createVisualisationObjectForUiElements", function() {
-        var mockVisCanvas;
-        beforeEach(function() {
-            mockVisCanvas = jQuery('<div class="vis front"></div><div class="vis front"></div>');
-            mockVisCanvas.visElement = function(position) {
-                return jQuery($('.vis.front', mockVisCanvas)[position]);
-            };
-        });
-
-        it("should create a visualisation for each element with 'vis' and 'front' classes", function() {
-            spyOn(jQuery.fn, 'find').andReturn(mockVisCanvas);
-            createVisualisationObjectsForUiElements();
-            expect(session.visualisations.length).toBe(2);
-        });
-        it("should set the id of ui elements to the id of the visualisations created for them", function() {
-            spyOn(jQuery.fn, 'find').andReturn(mockVisCanvas);
-            createVisualisationObjectsForUiElements();
-            var visElementId = parseInt(mockVisCanvas.visElement(0).attr('id'));
-            expect(visElementId).toBe(session.visualisations[0].id);
-        });
-    });
-
     describe("enableTrackingOfObjectProperties", function() {
         var mockPropertyButton;
         beforeEach(function() {
@@ -94,31 +38,5 @@ describe("index.js", function() {
                 });
             });
         });
-    });
-
-    describe("addSeriesToActiveVisualisation", function() {
-        beforeEach(function() {
-            session.addNewVisualisation().activate();
-        });
-
-        it("should add a series to visualisation", function(done) {
-            addSeriesToActiveVisualisation().then(function(visualisation) {
-                done();
-                expect(visualisation.series.length).toBe(1);
-            });
-        });
-        it("should add series only to the active visualisation", function(done) {
-            addSeriesToActiveVisualisation().then(function(visualisation) {
-                done();
-                expect(visualisation.id).toBe(session.activeVisualisation.id);
-            });
-        });
-        it("should add the series to the UI visualisation key", function(done) {
-            var addSeriesToVisKeySpy = spyOn(window, 'addSeriesToVisKey');
-            addSeriesToActiveVisualisation().then(function() {
-                done();
-                expect(addSeriesToVisKeySpy).toHaveBeenCalled();
-            });
-        })
     });
 });
