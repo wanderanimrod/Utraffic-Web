ed70f5b92aead901711bd958a21c85b10bf5d1c0
wanderanimrod <wanderanimrod@gmail.com>
2014-01-11 20:15:47 +0300

    [nim] Compressing api post code with jquery

--- app/static/js/sim_api.js
+++ app/static/js/sim_api.js
@@ -1,17 +1,8 @@
 function createSeries() {
     return new RSVP.Promise(function(resolve, reject) {
-        var request = new XMLHttpRequest();
-        request.open("POST", "http://127.0.0.1:5000/series/?debug=true", true);
-        request.onload = function() {
-            if(request.status === 201) {
-                var responseJson = JSON.parse(request.responseText);
-                var series = new Series(responseJson);
-                resolve(series);
-            }
-            else {
-                reject(Error(request.responseText));
-            }
-        };
-        request.send();
+        $.post('http://127.0.0.1:5000/series/?debug=true', function(response) {
+            var series = new Series(response);
+            resolve(series);
+        });
     });
 }
