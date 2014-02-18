f8618060f41e0fd50c6b842dd366ea9987b3e05c
wanderanimrod <wanderanimrod@gmail.com>
2014-02-19 00:19:45 +0300

    [nim] Started drawing axes

--- app/static/js/line_graph.js
+++ app/static/js/line_graph.js
@@ -1,7 +1,12 @@
 function drawLineGraphAxes(visId) {
-    var canvas = d3.visElementOfType(visId, '.visCanvas');
-    canvas.append('svg:svg')
-//        .attr('height', '100%');
+    var canvas = d3.visElementOfType(visId, '.visCanvas')
+        .append('svg:svg');
+    var yAxis = canvas.append("svg:line")
+        .attr('x1', 0)
+        .attr('y1', 0)
+        .attr('x2', 40)
+        .attr('y2', 40)
+        .style('stroke', 'rgb(6,120,155)');
 
 
     console.log('**************', canvas, '**************');
