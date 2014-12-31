933bcea7109b13741b1c70e129034a7562afeb2e
wanderanimrod <wanderanimrod@gmail.com>
2014-12-31 13:26:35 +0300

    Puts the models away. Too much state. Going to follow a more functional approach

--- app/scripts/patch-rsvp-vertx-problem.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Fixes issue with requiring rsvp. Discussion and fix gotten from here https://github.com/tildeio/rsvp.js/pull/339/files
-# Fix has already been merged. Version > 3.0.16
-
-cp ./rsvp.js ../node_modules/rsvp/dist/rsvp.js
